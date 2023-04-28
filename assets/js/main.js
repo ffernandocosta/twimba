7e4cf43dc9eee658b255c3fa43ac73d11a772580
Fernando Costa <ferndcosta353@gmail.com>
2023-04-28 17:25:34 -0300

    feat: Add like and retweet functionality to tweets
    The tweet feed now includes icons for liking and retweeting tweets. Clicking on these icons updates the tweet object in the 	weetsData array with the updated number of likes or retweets, and toggles the isLiked or isRetweeted property. This information is then used to update the feed display.
    
    BREAKING CHANGE: The tweet object in 	weetsData now includes two new properties: isLiked and isRetweeted.

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,45 @@ import { tweetsData } from "./data.js";
 const tweetInput = document.getElementById('tweet-input');
 const tweetBtn = document.getElementById('tweet-btn');
 
+document.addEventListener('click', function(e){
+    if(e.target.dataset.like){
+        handleLikeClick(e.target.dataset.like)
+    }
+    else if (e.target.dataset.retweet){
+        handleRetweetClick(e.target.dataset.retweet)
+    }
+})
+
+function handleLikeClick(tweetId){
+    const targetTweetObj = tweetsData.filter(function(tweet){
+        return tweet.uuid === tweetId
+    })[0]
+
+    if (targetTweetObj.isLiked){
+        targetTweetObj.likes--
+    }
+    else{
+        targetTweetObj.likes++
+    }
+    targetTweetObj.isLiked = !targetTweetObj.isLiked
+    render()
+}
+
+function handleRetweetClick(tweetId){
+    const targetTweetObj = tweetsData.filter(function(tweet){
+        return tweet.uuid === tweetId
+    })[0]
+
+    if (targetTweetObj.isRetweeted){
+        targetTweetObj.retweets--
+    }
+    else{
+        targetTweetObj.retweets++
+    }
+    targetTweetObj.isRetweeted = !targetTweetObj.isRetweeted
+    render()
+}
+
 function getHtmlFeed(){
     
     let htmlFeed = ``
